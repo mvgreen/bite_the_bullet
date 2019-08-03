1b6087b23d8a4105ed9420eecf5124584f791cbe
Rgiorno_Rgiovanna <thanekrios63@gmail.com>
2019-08-04 00:31:44 +0400

    Swarm boss basic movements

--- Assets/Scripts/EnemyPatternMovement.cs
+++ Assets/Scripts/EnemyPatternMovement.cs
@@ -10,11 +10,12 @@ public class EnemyPatternMovement : MonoBehaviour
     public MovementPattern.Orientations orientation;
     public MovementPattern.Modes mode;
     public Vector3 MovementAxis;
+    public float XBorder;
+    public float YBorder;
 
     private void Start()
     {
         MovementAxis = transform.position;
-        Debug.Log(gameObject.transform.position);
         direction = (int)MP.direction;
         orientation = MP.orientation;
         mode = MP.mode;
@@ -44,21 +45,23 @@ public class EnemyPatternMovement : MonoBehaviour
         }
 
         if (orientation == MovementPattern.Orientations.horizontal) {
+            if (Mathf.Abs(Mathf.Abs(transform.position.x) - XBorder) <= 0.1)
+                direction *= -1;
             v.x = coord1 + MP.delta* direction;
             v.y = coord2 + MovementAxis.y;
         }
         else
         {
+            if (Mathf.Abs(Mathf.Abs(transform.position.y) - YBorder) <= 0.1)
+                direction *= -1;
             v.x = coord2 + MovementAxis.x;
             v.y = coord1 + MP.delta * direction;
         }
 
-        GetComponent<Transform>().position = v;
+        transform.position = v;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(collision.gameObject);
+    private void OnTriggerEnter2D(Collider2D collision) { 
         if (collision.gameObject.tag == "Border Up" || collision.gameObject.tag == "Border Right" || collision.gameObject.tag == "Border Down" || collision.gameObject.tag == "Border Left")
         {
             
