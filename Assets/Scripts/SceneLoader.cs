4418ecd7463669e1cc498a81bab7beedcacf4d4f
Rgiorno_Rgiovanna <thanekrios63@gmail.com>
2019-08-03 10:15:32 +0400

    SceneLoader script and button functionality

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
