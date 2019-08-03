170696d357a2970f863b714347b1a68e61b4b8d7
mvgreen <mvgreen342@gmail.com>
2019-08-04 03:06:00 +0400

    Teleporting Boss with patterns.

---
+++ Assets/Scripts/TeleporterBoss/BossShootingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShootingController : MonoBehaviour
+{
+    public SpriteRenderer graphics;
+
+    public GameObject bulletPrefab;
+
+    public void shoot(Vector2 position, Vector2 direction)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
+        bullet.GetComponent<BossBulletMovement>().direction = direction.normalized;
+    }
+
+}
---
+++ Assets/Scripts/TeleporterBoss/TeleporterBossStateListener.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TeleporterBossBehaviour;
+
+public class TeleporterBossStateListener : MonoBehaviour
+{
+    TeleporterBossBehaviour shipBehaviour;
+    Animator animator;
+    System.Random rand;
+
+    void Start()
+    {
+        shipBehaviour = transform.parent.GetComponent<TeleporterBossBehaviour>();
+        animator = GetComponent<Animator>();
+        rand = new System.Random();
+    }
+   
+    public void onEnteringArena()
+    {
+        shipBehaviour.enterArena();
+    }
+
+    public void onRevlovingStarted()
+    {
+        shipBehaviour.startRevolving();
+        int nextMode = rand.Next() % 9 + 1;
+        animator.SetInteger("mode", nextMode);
+    }
+
+    public void onBlinkPreparing()
+    {
+        if (animator.GetInteger("mode") == 8)
+            return;
+        if (animator.GetInteger("mode") == 9)
+            shipBehaviour.markBlinkPosition(2);
+        else
+            shipBehaviour.markBlinkPosition(4);
+    }
+
+    public void onBlinkSpamStarted()
+    {
+        shipBehaviour.blinkOnce();
+        int nextMode = rand.Next() % 10;
+        animator.SetInteger("mode", nextMode);
+    }
+
+    public void onFastTeleport()
+    {
+        if (animator.GetInteger("mode") != 0)
+            shipBehaviour.hideOrbit();
+    }
+
+    public void onRush()
+    {
+        shipBehaviour.rush();
+        animator.SetInteger("mode", 9);
+    }
+
+    public void onShootStill()
+    {
+        shipBehaviour.shootStill();
+        int nextMode = rand.Next() % 10;
+        animator.SetInteger("mode", nextMode);
+    }
+
+    public void onPreparingRush()
+    {
+        shipBehaviour.prepareRush();
+    }
+
+    public void onAimingRush()
+    {
+        shipBehaviour.aimRush();
+    }
+
+    public void onPreparingRevolve()
+    {
+        if (animator.GetInteger("mode") == 0)
+            shipBehaviour.prepareRevolve();
+    }
+
+    public void onShootingCooldown()
+    {
+        shipBehaviour.stopShooting();
+    }
+}
