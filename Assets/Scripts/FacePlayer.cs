2399018057c23d01cf9dee6481eb0f20814537df
LunaticDancer <LunaticDancerGames@gmail.com>
2019-08-04 19:30:29 +0200

    graphics, a lot

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+
+    Transform player;
+    float scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        scale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(scale,scale,scale);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-scale,scale,scale);
+        }
+    }
+}
