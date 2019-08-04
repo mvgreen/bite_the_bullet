69b88366bccec94c899c725999768e5b44c5d1ec
LunaticDancer <LunaticDancerGames@gmail.com>
2019-08-04 18:59:55 +0200

    a lot of things

---
+++ Assets/Scripts/Revolve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolve : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles += new Vector3(0,0, speed*Time.deltaTime);
+        transform.GetChild(0).localEulerAngles = -transform.localEulerAngles;
+    }
+}
