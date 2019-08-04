87fada50cf93248e920146492e1e6a04ebf7a64e
mvgreen <mvgreen342@gmail.com>
2019-08-04 05:39:16 +0400

    Teleporting Boss adapted to standard prefabs + small tweak to player's bullets mechanics.

--- Assets/Scripts/TeleporterBoss/BossShootingController.cs
+++ Assets/Scripts/TeleporterBoss/BossShootingController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BossShootingController : MonoBehaviour
 {
     public SpriteRenderer graphics;
+    public float bulletSpeed;
 
     public GameObject bulletPrefab;
 
     public void shoot(Vector2 position, Vector2 direction)
     {
         GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
-        bullet.GetComponent<BossBulletMovement>().direction = direction.normalized;
+        bullet.GetComponent<EnemyBullet>().direction = direction.normalized;
+        bullet.GetComponent<EnemyBullet>().speed = bulletSpeed;
     }
 
 }
