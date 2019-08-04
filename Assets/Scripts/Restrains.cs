993e0bbc28fd63616b5214a6abc3a1e4b3e2f17b
Rgiorno_Rgiovanna <thanekrios63@gmail.com>
2019-08-04 22:33:46 +0400

    no message

---
+++ Assets/Scripts/Restrains.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restrains : MonoBehaviour
+{
+    public GameObject BossSprite;
+    public GameObject Boss;
+    public GameObject[] Bosses;
+    public static int BossesBeaten;
+    private void Start()
+    {
+        BossesBeaten = PlayerPrefs.GetInt("Bosses Beaten");
+        if(BossesBeaten < 1)
+        {
+            foreach(GameObject go in Bosses)
+            {
+                go.SetActive(false);
+            }
+            
+        }
+        if(BossesBeaten < 5)
+        {
+            Boss.SetActive(false);
+            BossSprite.SetActive(false);
+        }
+    }
+
+}
